92c7c5d61cdc8a374eb90ade4754e820b281d6dd
Jaume Zaragoza <jzaragoza@prompsit.com>
2021-07-12 12:22:52 +0200

    Update version

--- hardrules/bicleaner_hardrules.py
+++ hardrules/bicleaner_hardrules.py
@@ -30,7 +30,7 @@ except (SystemError, ImportError):
 __author__ = "Sergio Ortiz Rojas"
 __version__ = "Version 1.0 # 24/05/2021 # Separate hardrules package from Bicleaner # Jaume Zaragoza"
 __version__ = "Version 1.1 # 26/05/2021 # Load lm only when necessary # Jaume Zaragoza"
-__version__ = "Version 1.3 # 05/07/2021 # Discard bad encoding Ä, check only identical with alphabetic and discard empty sides # Jaume Zaragoza"
+__version__ = "Version 1.3 # 05/07/2021 # FastSpell, bad encoding Ä, check identical with alphabetic and discard empty sides # Jaume Zaragoza"
 
 logging_level = 0
 
