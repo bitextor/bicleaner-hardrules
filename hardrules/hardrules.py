791550dec32c14666c7b0043276561733c6f89b7
ZJaume <jzaragoza@prompsit.com>
2022-02-15 16:52:02 +0100

    Remove unused code and formatting changes

--- hardrules/hardrules.py
+++ hardrules/hardrules.py
@@ -1,7 +1,7 @@
-import unicodedata
 import logging
 import regex
 import sys
+from unicodedata import category as cat
 from fastspell import FastSpell
 from collections import OrderedDict
 from inspect import getmembers, signature
@@ -14,7 +14,7 @@ except (SystemError, ImportError):
     from lm import load_lm_filter
     from tokenizer import Tokenizer 
 
-tbl_non_alpha = [chr(i) for i in range(sys.maxunicode) if not unicodedata.category(chr(i)).startswith('L')]
+tbl_non_alpha = [chr(i) for i in range(sys.maxunicode) if not cat(chr(i)).startswith('L')]
 tbl_non_alpha = str.maketrans('', '', ''.join(tbl_non_alpha))
 regex_blank = regex.compile("[ \u00A0]")
 regex_alpha = regex.compile("[[:alpha:]]")
@@ -215,67 +215,6 @@ class Hardrules():
             return True
         return self.lm_filter.score(left, right) >= self.lm_threshold
 
-    '''
-    def c_different_language(self, left, right, left_lang, right_lang):
-        if left_lang =="nb":
-            left_lang="no"
-
-        if right_lang=="nb":
-            right_lang="no"
-            
-
-        l_reliable = False
-        l_bytes = 0
-        l_details = ()
-     
-        try:
-            l_reliable, l_bytes, l_details = pycld2.detect(left)
-        except:
-            return False # encoding error -> noise
-
-        r_reliable = False
-        r_bytes = 0
-        r_details = ()
-
-        try:
-            r_reliable, r_bytes, r_details = pycld2.detect(right)
-        except:
-            return False # encoding error -> noise
-            
-        if l_reliable and r_reliable and l_details[0][1] != r_details[0][1]:    
-            return True
-        elif not l_reliable or not r_reliable:
-            return True
-        else:
-            #both langs are reliable at this point, and the identified language is the same for left and right
-            identified = l_details[0][1]
-            if (identified in [left_lang, right_lang]  and {left_lang, right_lang} in similar_pairs):
-                return True
-            else:    
-                return False
-    '''
-    '''        
-    def c_reliable_long_language(self, sentence, language):
-        if language=="nb":
-            language = "no"
-            
-        reliable = False
-        bytes = 0
-        details = ()
-        
-        try:
-            reliable, bytes, details = pycld2.detect(sentence)
-        except:
-            return True # encoding error -> noise
-        
-        if len(sentence) > 30 and reliable and details[0][1] != language:
-            if {language, details[0][1]} in similar_pairs:
-                return True
-            else:
-                return False
-        else:
-            return True
-    '''
     def c_no_bad_encoding(self, sentence, side):
         lang = self.src_lang if side == 'left' else self.trg_lang
 
@@ -287,7 +226,7 @@ class Hardrules():
 
     def c_alpha(self, sentence, side):
         return len(regex_alpha.findall(sentence)) > 0
-        
+
     def c_majority_alpha(self, sentence, side):
         return float(len(regex_alpha.findall(sentence))) / float(len(sentence)) >= 0.5
 
@@ -297,19 +236,18 @@ class Hardrules():
     #def c_no_breadcrumbs(self, sentence, side):
     #    return len(regex_breadcrumbs.findall(sentence)) < 3
 
-
     def c_no_breadcrumbs1(self, sentence, side):
-        return len(regex_breadcrumbs1.findall(sentence)) < 3  
+        return len(regex_breadcrumbs1.findall(sentence)) < 333
 
     def c_no_breadcrumbs2(self, sentence, side):
-        return len(regex_breadcrumbs2.findall(sentence)) < 2  
+        return len(regex_breadcrumbs2.findall(sentence)) < 2
 
     def c_no_noise(self, sentence, side):
         return len(regex_unicode_noise.findall(sentence)) == 0
-        
+
     def c_no_space_noise(self, sentence, side):
         return len(regex_spaces_noise.findall(sentence)) == 0
-        
+
     def c_no_paren(self, sentence, side):
         return len(regex_paren.findall(sentence)) < 10
 
@@ -341,96 +279,5 @@ class Hardrules():
             tok = self.porn_tokenizer.tokenize(right.lower())
         else:
             raise Exception(f"c_no_porn rule needs 'sl' or 'tl' param, not {self.porn_removal_side}")
-        logging.debug(self.porn_removal.predict(self.porn_tokenizer.detokenize(tok))[0][0])
-        return self.porn_removal.predict(self.porn_tokenizer.detokenize(tok))[0][0] == '__label__negative'
 
-#def wrong_tu(left, right, args, lm_filter = None, porn_removal = None, porn_tokenizer = None, fastspell_src = None, fastspell_trg = None):
-#    if not left:
-#        return "c_no_empty(left)"
-#    if not right:
-#        return "c_no_empty(right)"
-#    if len(left) >= 1024:
-#        return "len(left) >= 1024"
-#    if len(right) >= 1024:
-#        return "len(right) >= 1024"
-#    elif not c_no_literals(["Re:"], left):
-#        return "c_no_literals(['Re:'], left)"
-#    elif not c_no_literals(["Re:"], right):
-#        return "c_no_literals(['Re:'], right)"            
-#    elif not args.disable_minimal_length and not (c_minimal_length(left) or c_minimal_length(right)):
-#        return "c_minimal_length(left) and c_minimal_length(right)"
-#    elif not (c_length(left, right) or c_length_bytes(left, right)): 
-#        return "c_length or c_length_bytes"
-#    elif not c_identical_alpha(left, right):
-#        return "c_identical_alpha"
-##    elif (not args.disable_lang_ident and not  c_different_language(left, right, args.source_lang, args.target_lang)):
-##        return "c_different_language"
-#    elif not c_majority_alpha(left):
-#        return "c_majority_alpha(left)"
-#    elif not c_majority_alpha(right):
-#        return "c_majority_alpha(right)"
-#    elif not c_no_urls(left):
-#        return "c_no_urls(left)"
-#    elif not c_no_urls(right):
-#        return "c_no_urls(right)"
-#    #elif not c_no_breadcrumbs(left):    
-#    #    return "c_no_breadcrumbs(left)"
-#    #elif not c_no_breadcrumbs(right):
-#    #    return "c_no_breadcrumbs(right)"
-#    elif not c_no_breadcrumbs1(left):
-#        return "c_no_breadcrumbs1(left)"
-#    elif not c_no_breadcrumbs1(right):
-#        return "c_no_breadcrumbs1(right)"
-#    elif not c_no_breadcrumbs2(left):
-#        return "c_no_breadcrumbs2(left)"
-#    elif not c_no_breadcrumbs2(right):
-#        return "c_no_breadcrumbs2(right)"       
-#    elif not c_no_glued_words(left):
-#        return "c_no_glued_words(left)"
-#    elif not c_no_glued_words(right):
-#        return "c_no_glued_words(right)"    
-#    elif args.source_lang in safe_noise_detection_langs and not c_no_noise(left):
-#        return "args.source_lang in safe_noise_detection_langs and not c_no_noise(left)" 
-#    elif args.target_lang in safe_noise_detection_langs and not c_no_noise(right):
-#        return "args.target_lang in safe_noise_detection_langs and not c_no_noise(right)"
-#    elif not c_no_space_noise(left):
-#        return "c_no_space_noise(left)"
-#    elif not c_no_space_noise(right):
-#        return "c_no_space_noise(right)"
-#    elif not c_no_paren(left):
-#        return "c_no_paren(left)"
-#    elif not c_no_paren(right):
-#        return "c_no_paren(right)"
-#    elif not c_unwanted(left):
-#        return "c_unwanted(left)"
-#    elif not c_unwanted(right):
-#        return "c_unwanted(right)"
-#    elif not c_inconditional(left):
-#        return "c_inconditional(left)"
-#    elif not c_inconditional(right):
-#        return "c_inconditional(right)"
-#    elif not c_no_escaped_unicode(left):
-#        return "c_no_escaped_unicode(left)"
-#    elif not c_no_escaped_unicode(right):
-#        return "c_no_escaped_unicode(right)"
-#    elif not c_no_literals(["{{", "%s", "}}"], left):
-#        return 'c_no_literals(["{{", "%s", "}}"], left)'
-#    elif not c_no_literals(["{{", "%s", "}}"], right):
-#        return 'c_no_literals(["{{", "%s", "}}"], right)'
-#    elif not c_no_bad_encoding(left, args.source_lang) and not c_no_bad_encoding(right, args.target_lang):
-#        return 'c_no_bad_encoding(["Â","Ã"])'
-#    elif left.istitle() and right.istitle():
-#        return 'left.istitle() and right.istitle()'
-##    elif (not args.disable_lang_ident and not  c_reliable_long_language(left, args.source_lang)):
-##        return "c_reliable_long_language(left, sourcelang)"
-##    elif (not args.disable_lang_ident and  not c_reliable_long_language(right, args.target_lang)):
-##        return "c_reliable_long_language(right, targetlang)"
-#    elif (not args.disable_lang_ident and not  fastspell_src.getlang(left)==args.source_lang):
-#        return "c_wrong_language(left, sourcelang)"
-#    elif (not args.disable_lang_ident and  not fastspell_trg.getlang(right)==args.target_lang):
-#        return "c_wrong_language(right, targetlang)"
-#    elif not args.disable_porn_removal and porn_removal != None and not c_no_porn(left, right, porn_removal, args.metadata_yaml['porn_removal_side'], porn_tokenizer):
-#        return "c_no_porn"
-#    elif  args.disable_lm_filter == False and lm_filter != None and lm_filter.score(left, right) < args.lm_threshold:
-#        return "lm_filter.score(left, right) < args.lm_threshold"
-#    return False
+        return self.porn_removal.predict(self.porn_tokenizer.detokenize(tok))[0][0] == '__label__negative'
