dd8a0671a544e294d7f1d64c0d07b2aa13efc226
ZJaume <jzaragoza@prompsit.com>
2022-07-15 10:18:49 +0200

    Fix stdin being closed randomly

--- hardrules/unicodedata2.py
+++ hardrules/unicodedata2.py
@@ -605,33 +605,29 @@ def script_family(chr):
         return 'Phoenician'    
     else:
         return chr_script    
-    
-        
-    
-    
 
-def _compile_scripts_txt():
-    # build indexes from 'scripts.txt'
-
-    idx = []
-    names = []
-    cats = []
-
-    import urllib2, re, textwrap
-
-    url = 'http://www.unicode.org/Public/UNIDATA/Scripts.txt'
-    f = urllib2.urlopen(url)
-    for ln in f:
-        p = re.findall(r'([0-9A-F]+)(?:\.\.([0-9A-F]+))?\W+(\w+)\s*#\s*(\w+)', ln)
-        if p:
-            a, b, name, cat = p[0]
-            if name not in names:
-                names.append(name)
-            if cat not in cats:
-                cats.append(cat)
-            idx.append((int(a, 16), int(b or a, 16), names.index(name), cats.index(cat)))
-    idx.sort()
-
+#def _compile_scripts_txt():
+#    # build indexes from 'scripts.txt'
+#
+#    idx = []
+#    names = []
+#    cats = []
+#
+#    import urllib2, re, textwrap
+#
+#    url = 'http://www.unicode.org/Public/UNIDATA/Scripts.txt'
+#    f = urllib2.urlopen(url)
+#    for ln in f:
+#        p = re.findall(r'([0-9A-F]+)(?:\.\.([0-9A-F]+))?\W+(\w+)\s*#\s*(\w+)', ln)
+#        if p:
+#            a, b, name, cat = p[0]
+#            if name not in names:
+#                names.append(name)
+#            if cat not in cats:
+#                cats.append(cat)
+#            idx.append((int(a, 16), int(b or a, 16), names.index(name), cats.index(cat)))
+#    idx.sort()
+#
 #    print 'script_data = {\n"names":%s,\n"cats":%s,\n"idx":[\n%s\n]}' % (
 #        '\n'.join(textwrap.wrap(repr(names), 80)),
 #        '\n'.join(textwrap.wrap(repr(cats), 80)),
