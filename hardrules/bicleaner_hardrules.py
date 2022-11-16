bcb5a28ec2a89ad4fe0142ba0772b2a57ab6b424
ZJaume <jzaragoza@prompsit.com>
2022-11-16 10:23:19 +0100

    Freeze some requirements and update FastSpell

--- hardrules/bicleaner_hardrules.py
+++ hardrules/bicleaner_hardrules.py
@@ -96,7 +96,7 @@ def initialization():
     if not args.disable_lang_ident:
         # Load a FastSpell objet to trigger download of fasttext langid
         # before running hardrules
-        fsobj = FastSpell.FastSpell("en", mode="cons")
+        fsobj = FastSpell("en", mode="aggr")
 
     #Try loading metadata for LM filtering and porn removal
     if not (args.disable_lm_filter and args.disable_porn_removal) and args.metadata != None:
--- hardrules/hardrules.py
+++ hardrules/hardrules.py
@@ -113,8 +113,8 @@ class Hardrules():
 
         # Load FastSpell
         if not args.disable_lang_ident:
-            self.fastspell_src = FastSpell.FastSpell(args.source_lang, mode="aggr")
-            self.fastspell_trg = FastSpell.FastSpell(args.target_lang, mode="aggr")
+            self.fastspell_src = FastSpell(args.source_lang, mode="aggr")
+            self.fastspell_trg = FastSpell(args.target_lang, mode="aggr")
         else:
             self.fastspell_src = None
             self.fastspell_trg = None
